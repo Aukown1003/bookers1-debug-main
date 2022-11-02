b287d9f8c007f1fb95a7c1bf1c65b74f13e594bc
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-11-02 05:37:46 +0000

    [Finish]boolers1-debug-main

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,8 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    # if @book.update() ← ストロングパラメータ抜け
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
